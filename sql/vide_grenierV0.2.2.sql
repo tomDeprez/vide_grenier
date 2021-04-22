7a013b947c92600ecb665b642240b4809bfdb806
mayol <mayolboucaud@gmail.com>
2021-04-22 09:55:54 +0200

    bdd avec données test

--- sql/vide_grenierV0.2.2.sql
+++ sql/vide_grenierV0.2.2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 21 avr. 2021 à 09:36
+-- Généré le : jeu. 22 avr. 2021 à 07:50
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `vide_grenier3`
+-- Base de données : `vide_grenier4`
 --
 
 DELIMITER $$
@@ -34,12 +34,12 @@ END$$
 DROP PROCEDURE IF EXISTS `inscriptionMailing`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `inscriptionMailing` (IN `mail` VARCHAR(50))  MODIFIES SQL DATA
 BEGIN
-    INSERT INTO mailing_list
+    INSERT INTO mailing_list (MAIL_ML)
     SELECT mail
-    FROM (SELECT MAIL_ML) t
+    FROM (SELECT mail) t
     WHERE NOT EXISTS (SELECT 1
                       FROM mailing_list ml
-                      WHERE ml.MAIL_ML = t.MAIL_ML
+                      WHERE ml.MAIL_ML = t.mail
         );
 END$$
 
@@ -64,7 +64,14 @@ CREATE TABLE IF NOT EXISTS `attestationhonneur` (
                                                     `NUMPLAQIMM_AH` text,
                                                     PRIMARY KEY (`ID_AH`),
                                                     KEY `ID_HOROD` (`ID_HOROD`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=111112 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `attestationhonneur`
+--
+
+INSERT INTO `attestationhonneur` (`ID_AH`, `ID_HOROD`, `DATENAIS_AH`, `DEPTNAIS_AH`, `VILLENAIS_AH`, `NUMCNI_AH`, `DATEDELIVRCNI_AH`, `EMETCNI_AH`, `NUMPLAQIMM_AH`) VALUES
+(111111, 1111111, '1980-10-01', '69', 'Lyon', '200769180525', '2010-11-02', 'Préfecture du Rhône', 'AAA-000-AA');
 
 -- --------------------------------------------------------
 
@@ -83,7 +90,7 @@ CREATE TABLE IF NOT EXISTS `exposant` (
                                           `ADR_EXP` text NOT NULL,
                                           `CP_EXP` decimal(8,0) NOT NULL,
                                           `VILLE_EXP` text NOT NULL,
-                                          `TEL_EXP` decimal(10,0) NOT NULL,
+                                          `TEL_EXP` varchar(10) NOT NULL,
                                           `EMAIL_EXP` text NOT NULL,
                                           `COMMENT_EXP` text,
                                           PRIMARY KEY (`ID_EXP`),
@@ -105,7 +112,14 @@ CREATE TABLE IF NOT EXISTS `horodatage` (
                                             `DATE_HOROD` date NOT NULL,
                                             `HEURE_HOROD` time NOT NULL,
                                             PRIMARY KEY (`ID_HOROD`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1111112 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `horodatage`
+--
+
+INSERT INTO `horodatage` (`ID_HOROD`, `IP_HOROD`, `DATE_HOROD`, `HEURE_HOROD`) VALUES
+(1111111, '172.16.254.1', '2019-03-15', '22:21:20');
 
 -- --------------------------------------------------------
 
@@ -118,7 +132,17 @@ CREATE TABLE IF NOT EXISTS `mailing_list` (
                                               `ID_ML` int(11) NOT NULL AUTO_INCREMENT,
                                               `MAIL_ML` varchar(50) DEFAULT NULL,
                                               PRIMARY KEY (`ID_ML`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `mailing_list`
+--
+
+INSERT INTO `mailing_list` (`ID_ML`, `MAIL_ML`) VALUES
+(5, 't@gmail.com'),
+(6, 'a@gmail.com'),
+(7, 'aa@gmail.com'),
+(8, 'bb@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -151,7 +175,15 @@ CREATE TABLE IF NOT EXISTS `role` (
                                       `MEMB_ROL` text NOT NULL,
                                       `VISIT_ROL` text NOT NULL,
                                       PRIMARY KEY (`ID_ROL`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `role`
+--
+
+INSERT INTO `role` (`ID_ROL`, `ADMIN_ROL`, `MEMB_ROL`, `VISIT_ROL`) VALUES
+(1, 'null', 'null', 'visiteur'),
+(2, 'administrateur', 'null', 'null');
 
 -- --------------------------------------------------------
 
@@ -166,10 +198,19 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
                                              `NOM_UTIL` text NOT NULL,
                                              `MDP_UTIL` text NOT NULL,
                                              `EMAIL_UTIL` text NOT NULL,
-                                             `TEL_UTIL` decimal(10,0) DEFAULT NULL,
+                                             `TEL_UTIL` varchar(10) DEFAULT NULL,
                                              PRIMARY KEY (`ID_UTIL`),
                                              KEY `ID_ROL` (`ID_ROL`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`ID_UTIL`, `ID_ROL`, `NOM_UTIL`, `MDP_UTIL`, `EMAIL_UTIL`, `TEL_UTIL`) VALUES
+(1, 1, 'TOTO', '000000', 't@gmail.com', '0606060606'),
+(4, 2, 'aa', '000000', 'aa@gmail.com', '0606060606'),
+(5, 1, 'bb', '000000', 'bb@gmail.com', '0606060606');
 
 -- --------------------------------------------------------
 
@@ -188,7 +229,14 @@ CREATE TABLE IF NOT EXISTS `videgrenier` (
                                              `NBREEMPLRESTREEL_VG` decimal(8,0) NOT NULL,
                                              `NBREPARTICIP_VG` decimal(8,0) NOT NULL,
                                              PRIMARY KEY (`ID_VG`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11112 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `videgrenier`
+--
+
+INSERT INTO `videgrenier` (`ID_VG`, `DATE_VG`, `PRIXEMPL_VG`, `NBREEMPLINIT_VG`, `NBREEMPLINDISPO_VG`, `NOMBRE_D_EMPLACEMENTS_RESTANTS_TEMPORAIRES_`, `NBREEMPLRESTREEL_VG`, `NBREPARTICIP_VG`) VALUES
+(11111, '2020-07-25', 10.00, '150', '30', '117', '147', '1');
 
 --
 -- Contraintes pour les tables déchargées
