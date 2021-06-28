9ad21923e74f645e5521bc635316cb2d208bd361
mayol <mayolboucaud@gmail.com>
2021-06-28 12:11:07 +0200

    Réservation

--- php/verif_resa.php
+++ php/verif_resa.php
@@ -59,8 +59,10 @@ if (isset($_SESSION["id_util"]) && isset($_GET['idVG']) && $_POST['nom'] !="" &&
             $numCNI = htmlspecialchars($_POST['numCNI']);
             $dateCNI = htmlspecialchars($_POST['dateCNI']);
             $parCNI = htmlspecialchars($_POST['parCNI']);
+            $immatriculation = htmlspecialchars($_POST['immatriculation']);
             $portable = htmlspecialchars($_POST['portable']);
             $nbrEmplacement = htmlspecialchars($_POST['nbrEmplacement']);
+            $commentaire = htmlspecialchars($_POST['remarque']);
 
 
             $sql0 = "SELECT NBREEMPLINDISPO_VG FROM videgrenier WHERE id_vg = :id";
@@ -85,33 +87,13 @@ if (isset($_SESSION["id_util"]) && isset($_GET['idVG']) && $_POST['nom'] !="" &&
             $resultat_update->bindParam(':restant', $nbrPlaceRestantApresResa);
             $resultat_update->execute();
 
-            if ($_POST['immatriculation'] == "") {
-
-                $tempImma = "";
-                $valueImma = "";
-            } else {
-
-                $tempImma = ", IMMATRICULATION_RESA";
-                $valueImma = ", :immatricul";
-            }
-
-            if ($_POST['remarque'] == "") {
-
-                $tempInfo = "";
-                $valueInfo = "";
-            } else {
-
-                $tempInfo = ", INFO_RESA";
-                $valueInfo = ", :info";
-            }
-
             $type = 'En ligne';
             $statut = 'En attente';
             $numPlace = 1;
 
 
 
-            $insert_resa = "INSERT INTO reservation (ID_VG, TYPEPAIEMENT_RES, STATUTRESERVATION_RES, NUMEMPLATTRIBUE_RES) VALUES (:id_vg, :type, :statut, :numPlace)";
+            $insert_resa = "INSERT INTO reservation (ID_VG, TYPEPAIEMENT_RES, STATUTRESERVATION_RES, NUMEMPLATTRIBUE_RES, NBREEMPLRESERVE_RES) VALUES (:id_vg, :type, :statut, :numPlace, :nbPlace)";
             $resultat_insert = $base->prepare($insert_resa);
 
 
@@ -121,39 +103,88 @@ if (isset($_SESSION["id_util"]) && isset($_GET['idVG']) && $_POST['nom'] !="" &&
             $resultat_insert->bindParam(':id_vg', $id_vg);
             $resultat_insert->bindParam(':type', $type);
             $resultat_insert->bindParam(':numPlace', $numPlace);
+            $resultat_insert->bindParam(':nbPlace', $nbrEmplacement);
             $resultat_insert->bindParam(':statut', $statut);
 
             $resultat_insert->execute();
             $id_reservation = $base->lastInsertId();
             $base->commit();
 
-            $insert_exposant = "INSERT INTO exposant (ID_RES, ID_AH, ID_UTIL, NOM_EXP, PRENOM_EXP, ADR_EXP, CP_EXP, VILLE_EXP, TEL_EXP, EMAIL_EXP, COMMENT_EXP) VALUES (:idReservation)";
+            $insert_horodatage = "INSERT INTO horodatage (IP_HOROD, DATE_HOROD, HEURE_HOROD) VALUES (:ipHorod, :DateHorod, :HeureHorod)";
+            $resultat_horodatage = $base->prepare($insert_horodatage);
 
-            $resultat_insert->bindParam(':id', $_SESSION["id_util"]);
-            $resultat_insert->bindParam(':idReservation', $id_reservation);
-            $resultat_insert->bindParam(':nom', $nom);
-            $resultat_insert->bindParam(':prenom', $prenom);
-            $resultat_insert->bindParam(':mail', $mail);
-            $resultat_insert->bindParam(':addresse', $addresse);
-            $resultat_insert->bindParam(':postal', $postal);
-            $resultat_insert->bindParam(':ville', $ville);
-            $resultat_insert->bindParam(':cni', $numCNI);
-            $resultat_insert->bindParam(':delivrer', $dateCNI);
-            $resultat_insert->bindParam(':par', $parCNI);
-            $resultat_insert->bindParam(':portable', $portable);
-            $resultat_insert->bindParam(':nbr', $nbrEmplacement);
+            $localIP = $_SERVER['REMOTE_ADDR'];
 
-            if ($_POST['immatriculation'] != "") {
-
-                $immatriculation = htmlspecialchars($_POST['immatriculation']);
-                $resultat_insert->bindParam(':immatricul', $immatriculation);
+            if($localIP == '::1')
+            {
+                $localIP = '127.0.0.1';
             }
 
-            if ($_POST['remarque'] != "") {
+            date_default_timezone_set('UTC');
+            $today = date("Y-m-d");
+            $hour = date("H:i:s");
 
-                $remarque = htmlspecialchars($_POST['remarque']);
-                $resultat_insert->bindParam(':info', $remarque);
-            }
+            // poura récupérer l'id de l'horodatage
+            $base->beginTransaction();
+
+            $resultat_horodatage->bindParam(':ipHorod', $localIP);
+            $resultat_horodatage->bindParam(':DateHorod', $today);
+            $resultat_horodatage->bindParam(':HeureHorod', $hour);
+
+            $resultat_horodatage->execute();
+            $id_horodatage = $base->lastInsertId();
+            $base->commit();
+
+            $insert_AttestationH = "INSERT INTO attestationhonneur (ID_HOROD, DATENAIS_AH, DEPTNAIS_AH, VILLENAIS_AH, NUMCNI_AH, DATEDELIVRCNI_AH, EMETCNI_AH, NUMPLAQIMM_AH) VALUES (:idH, null, null, null, :numCNI, :dateCNI, :parCNI, :immatriculation)";
+            $resultat_AttestationH = $base->prepare($insert_AttestationH);
+
+            // poura récupérer l'id de l'attestation honneurs
+            $base->beginTransaction();
+
+            $resultat_AttestationH->bindParam(':idH', $id_horodatage);
+            $resultat_AttestationH->bindParam(':numCNI', $numCNI);
+            $resultat_AttestationH->bindParam(':dateCNI', $dateCNI);
+            $resultat_AttestationH->bindParam(':parCNI', $parCNI);
+            $resultat_AttestationH->bindParam(':immatriculation', $immatriculation);
+
+            $resultat_AttestationH->execute();
+            $id_ah = $base->lastInsertId();
+            $base->commit();
+
+            $insert_exposant = "INSERT INTO exposant (ID_RES, ID_AH, ID_UTIL, NOM_EXP, PRENOM_EXP, ADR_EXP, CP_EXP, VILLE_EXP, TEL_EXP, EMAIL_EXP, COMMENT_EXP) VALUES (:idReservation, :idAH, :id, :nom, :prenom, :addresse, :postal, :ville, :portable, :mail, :commentaire)";
+            $result_exposant = $base->prepare($insert_exposant);
+
+            $result_exposant->bindParam(':id', $_SESSION["id_util"]);
+            $result_exposant->bindParam(':idReservation', $id_reservation);
+            $result_exposant->bindParam(':idAH', $id_ah);
+            $result_exposant->bindParam(':nom', $nom);
+            $result_exposant->bindParam(':prenom', $prenom);
+            $result_exposant->bindParam(':mail', $mail);
+            $result_exposant->bindParam(':addresse', $addresse);
+            $result_exposant->bindParam(':postal', $postal);
+            $result_exposant->bindParam(':ville', $ville);
+//            $resultat_insert->bindParam(':cni', $numCNI);
+//            $resultat_insert->bindParam(':delivrer', $dateCNI);
+//            $result_exposant->bindParam(':par', $parCNI);
+            $result_exposant->bindParam(':portable', $portable);
+            $result_exposant->bindParam(':commentaire', $commentaire);
+//            $resultat_insert->bindParam(':nbr', $nbrEmplacement);
+//
+//
+//            var_dump($result_exposant);
+            $result_exposant->execute();
+
+//            if ($_POST['immatriculation'] != "") {
+//
+//                $immatriculation = htmlspecialchars($_POST['immatriculation']);
+//                $resultat_insert->bindParam(':immatricul', $immatriculation);
+//            }
+//
+//            if ($_POST['remarque'] != "") {
+//
+//                $remarque = htmlspecialchars($_POST['remarque']);
+//                $resultat_insert->bindParam(':info', $remarque);
+//            }
 
 
             echo "<section id=\"resaValidée\" class=\"boxSite\">Votre réservation est validée!<br/>
