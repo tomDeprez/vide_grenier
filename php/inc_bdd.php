74dd0dda5e609b630f0cf25cecc4dda8f0fb6c0d
momthex <pierrereynaud8@gmail.com>
2021-07-05 20:33:43 +0200

    Signalement des emplacements réservés

--- php/inc_bdd.php
+++ php/inc_bdd.php
@@ -1,10 +1,5 @@
 <?php
-
-<<<<<<< HEAD
-    $base = new PDO('mysql:host=localhost:3309; dbname=vide_grenier', 'root', '');
-=======
     $base = new PDO('mysql:host=localhost:3307; dbname=vide_grenier', 'root', '');
->>>>>>> master
     $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $base->exec("SET NAMES utf8");
 
