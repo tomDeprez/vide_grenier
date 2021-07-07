76ad2dac5b4280c4565067a8d19c9f365376c9a7
TomTom <tom_deprez@hotmail.fr>
2021-07-07 17:10:28 +0200

    connexion

--- php/inc_bdd.php
+++ php/inc_bdd.php
@@ -1,5 +1,5 @@
 <?php
-    $base = new PDO('mysql:host=localhost; dbname=vide_grenier', 'root', 'root');
+    $base = new PDO('mysql:host=localhost:3306; dbname=vide_grenier', 'root', 'root');
     $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $base->exec("SET NAMES utf8");
 
